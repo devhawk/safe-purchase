eb8bf13eabee29a1bbd027e75676464794e7efbe
Harry <harrypierson@ngd.neo.org>
2020-09-03 17:20:34 -0700

    update checkpoint generation

--- generate-checkpoints.ps1
+++ generate-checkpoints.ps1
@@ -1,8 +1,8 @@
-dotnet build
+dotnet build ./contract
 dotnet nxp3 reset -f
 dotnet nxp3 contract deploy ./contract/bin/Debug/netstandard2.1/safe-purchase.nef genesis
-dotnet nxp3 transfer gas 1000 genesis buyer
-dotnet nxp3 transfer gas 1000 genesis seller
+dotnet nxp3 transfer gas 10000 genesis buyer
+dotnet nxp3 transfer gas 10000 genesis seller
 dotnet nxp3 checkpoint create checkpoints/create-sale -f
 dotnet nxp3 contract invoke ./invoke-files/create-sale.neo-invoke.json seller
 dotnet nxp3 checkpoint create checkpoints/buyer-deposit -f
